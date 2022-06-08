6298abdf1eefa8dafdf4881011b2213cf283d97f
Mateus Silva <mateussilva651@gmail.com>
2022-06-08 19:00:22 -0300

    criação de tabelas e migrations

---
+++ ORM/api/routes/pessoasRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const PessoaController = require("../controllers/PessoaController");
+
+const router = Router();
+
+router
+  .get("/pessoas", PessoaController.pegaTodasAsPessoas)
+  .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
+  .post("/pessoas", PessoaController.criarPessoa)
+  .put("/pessoas/:id", PessoaController.atualizaPessoa)
+  .delete("/pessoas/:id", PessoaController.apagaPessoa);
+
+module.exports = router;

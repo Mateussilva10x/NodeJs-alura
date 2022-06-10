c47c13603b388aa257f6adacb231c458848cfe69
Mateus Silva <mateussilva651@gmail.com>
2022-06-10 14:37:33 -0300

    criando services

---
+++ ORM/api/services/TurmasServices.js
@@ -0,0 +1,11 @@
+const Services = require("./Services");
+
+class PessoasServices extends Services {
+  constructor() {
+    super("Pessoas");
+  }
+
+  //metodos especificos
+}
+
+module.exports = PessoasServices;
